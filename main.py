0f24dc1e95a0478f29e55544ca52debe19fbef64
Anna M <83anei@gmail.com>
2021-10-13 19:54:49 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from faker import Faker
+f = Faker()
+
+
+class Card:
+    def __init__(self, name, surname, company_name, occupation, email):
+        self.name = name
+        self.surname = surname
+        self.company_name = company_name
+        self.occupation = occupation
+        self.email = email
+
+    def __str__(self):
+        return f"Name: {self.name}, Surname: {self.surname}, Company name: {self.company_name}, Occupation: {self.occupation}, E-mail address: {self.email}"
+
+
+name_cards = []
+for i in range(5):
+    name_cards.append(Card(f.first_name(), f.last_name(), f.company(), f.job(), f.email()))
+    print(name_cards[i])
