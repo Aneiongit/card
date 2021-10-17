3ed453fe54c1f8ff2af6f275cca437fde2962d64
Anna M <83anei@gmail.com>
2021-10-17 16:23:20 +0100

    Change

--- main.py
+++ main.py
@@ -11,29 +11,30 @@ class Card:
         self.email = email
 
     def __str__(self):
-        return f"Name: {self.name}, Surname: {self.surname}, E-mail address: {self.email}"
+        return f'{self.name} {self.surname} {self.email}'
+
+    def contact(self):
+        return f'Kontaktuje sie z {self.name} {self.surname} {self.occupation} {self.email}'
+
+    @property
+    def len(self):
+        return f'{len(self.name)} {len(self.surname)}'
 
 
-name_cards = []
-for i in range(5):
-    name_cards.append(Card(f.first_name(), f.last_name(), f.company(), f.job(), f.email()))
-    print(name_cards[i])
+c1 = Card(name=f.first_name(), surname=f.last_name(), company_name=f.company(), occupation=f.job(), email=f.email())
+c2 = Card(name=f.first_name(), surname=f.last_name(), company_name=f.company(), occupation=f.job(), email=f.email())
+c3 = Card(name=f.first_name(), surname=f.last_name(), company_name=f.company(), occupation=f.job(), email=f.email())
+c4 = Card(name=f.first_name(), surname=f.last_name(), company_name=f.company(), occupation=f.job(), email=f.email())
+c5 = Card(name=f.first_name(), surname=f.last_name(), company_name=f.company(), occupation=f.job(), email=f.email())
 
-print()
-for i in range(5):
-    name_cards.append(Card(f.first_name(), f.last_name(), f.company(), f.job(), f.email()))
-    sorted_by_first_name = sorted(name_cards, key=lambda card: card.name)
-    print(sorted_by_first_name[i])
-print()
+cards = [c1, c2, c3, c4, c5]
 
-for i in range(5):
-    name_cards.append(Card(f.first_name(), f.last_name(), f.company(), f.job(), f.email()))
-    sorted_by_surname = sorted(name_cards, key=lambda card: card.surname)
-    print(sorted_by_surname[i])
-print()
+by_name = sorted(cards, key=lambda card: card.name)
+by_surname = sorted(cards, key=lambda card: card.surname)
+by_email = sorted(cards, key=lambda card: card.email)
 
-for i in range(5):
-    name_cards.append(Card(f.first_name(), f.last_name(), f.company(), f.job(), f.email()))
-    sorted_by_email = sorted(name_cards, key=lambda card: card.email)
-    print(sorted_by_email[i])
-print()
+for card in cards:
+    print(card)
+    print(card.contact())
+    print(card.len)
+
