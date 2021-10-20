76de28f3582449fb6ea0ac9a027395a134881bab
Anna M <83anei@gmail.com>
2021-10-20 17:01:31 +0100

    Add inheritance

--- main.py
+++ main.py
@@ -3,43 +3,62 @@ from faker import Faker
 f = Faker()
 
 
-class Card:
-    def __init__(self, name, surname, company_name, occupation, email):
+class BaseContact:
+    def __init__(self, name, surname, phone, email):
         self.name = name
         self.surname = surname
-        self.company_name = company_name
-        self.occupation = occupation
+        self.phone = phone
         self.email = email
 
     def __str__(self):
-        return f'{self.name} {self.surname} {self.email}'
+        return f'{self.name} {self.surname} {self.phone} {self.email}'
 
     def contact(self):
-        return f'Kontaktuje sie z {self.name} {self.surname} {self.occupation} {self.email}'
+        return f'Wybieram numer {self.phone} i dzwonię do {self.name} {self.surname}.'
 
     @property
     def len(self):
         return f'{len(self.name)} {len(self.surname)}'
 
 
-cards = []
-for i in range(5):
-    cards.append(Card(f.first_name(), f.last_name(), f.company(), f.job(), f.email()))
-    print(cards[i])
+class BusinessContact(BaseContact):
+    def __init__(self, company_name, occupation, work_phone, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.company_name = company_name
+        self.occupation = occupation
+        self.work_phone = work_phone
+
+    def __str__(self):
+        return f'{self.name} {self.surname} {self.phone} {self.email} {self.company_name} ' \
+               f'{self.occupation} {self.work_phone}'
+
+    def contact(self):
+        return f'Wybieram numer {self.work_phone} i dzwonię do {self.name} {self.surname}'
+
+    @property
+    def len(self):
+        return f'{len(self.name)} {len(self.surname)}'
 
 
-def print_by_order(cards, func):
-    ordered_cards = sorted(cards, key=func)
-    for element in ordered_cards:
-        print(element)
+def create_contacts(type, cards):
+    card = []
+    if type == "base":
+        for i in range(cards):
+            card.append(BaseContact(f.first_name(), f.last_name(), f.phone_number(), f.email()))
+            print(card[i])
+            print()
+
+    elif type == "business":
+        for i in range(cards):
+            card.append(BusinessContact(f.job(), f.company(), f.phone_number(), f.first_name(), f.last_name(),
+                                        f.phone_number(), f.email()))
+            print(card[i])
+
+    else:
+        print("Podano nieprawidlowy typ.")
 
 
-print_by_order(cards, lambda x: x.name)
-print_by_order(cards, lambda x: x.surname)
-print_by_order(cards, lambda x: x.email)
+type = input("Podaj typ (base or business): ")
+cards = int(input("Podaj ilość: "))
+print(create_contacts(type, cards))
 
-print()
-for card in cards:
-    print(card)
-    print(card.contact())
-    print(card.len)
